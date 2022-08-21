974e6b5747f29e03c377b265c9381d1e46eb8717
James Hargreaves <james.hargreaves@thetradedesk.com>
2022-08-22 00:34:53 +0100

    config update

--- createConfigs.py
+++ createConfigs.py
@@ -30,7 +30,7 @@ def main():
     base_config = yaml.safe_load(open("./baseConfig.yaml"))
     configs = {}
     totalNumberPartitions = 10
-    for repeatNumber in tqdm(range(500)):
+    for repeatNumber in tqdm(range(500,1000)):
         for i in range(totalNumberPartitions):
             id = "{}_{}_{}_{}_{}".format(base_config['modelType'], totalNumberPartitions,
                                          base_config['totalTrainingSize'],
